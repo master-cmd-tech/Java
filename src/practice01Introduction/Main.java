4099fbb013fc23bb53aecc53db15204a68fe2e97
master-cmd-tech <abdrahmanov01122006@gmail.com>
2026-04-13 17:22:41 +0500

    Introduction

---
+++ src/practice01Introduction/Main.java
@@ -0,0 +1,20 @@
+package practice01Introduction;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Comments
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+         */
+
+        System.out.println("I like dumplings!");
+        System.out.print("It is really good!\n");
+        System.out.print("Buy me dumplings!");
+        System.out.println();
+    }
+}
